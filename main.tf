9bbe10c1f74b3ae8143f0392aa928feb16f14458
klimh <klimgudzenko@gmail.com>
2026-04-14 15:23:06 +0200

    build: data lake creating

--- main.tf
+++ main.tf
@@ -3,11 +3,28 @@ provider "google" {
   region  = "europe-central2"
 }
 
+data "google_project" "project" {}
+
 resource "google_artifact_registry_repository" "nbp_repo" {
   project       = "currency-prices-data-pipeline"
   location      = "europe-central2"
   repository_id = "nbp-app-repo"
   format        = "DOCKER"
+}
+
+resource "google_storage_bucket" "nbp_data_lake" {
+  name          = "nbp-data-lake-${var.project_id}"
+  location      = "EUROPE-CENTRAL2"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 30
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
 
 resource "google_cloud_run_v2_service" "nbp_service" {
@@ -33,4 +50,10 @@ resource "google_cloud_run_v2_service_iam_binding" "public" {
   location = google_cloud_run_v2_service.nbp_service.location
   role     = "roles/run.invoker"
   members  = ["allUsers"]
+}
+
+resource "google_storage_bucket_iam_member" "run_storage_writer" {
+  bucket = google_storage_bucket.nbp_data_lake.name
+  role   = "roles/storage.objectCreator"
+  member = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
 }
